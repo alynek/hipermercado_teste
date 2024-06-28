8fd9e154e017a950dd84f95738b9fe54c8e9842d
alynek <alyne0x67@gmail.com>
2024-06-27 23:11:53 -0300

    Cria testes para as classes filhas

--- Questao2/CalculoCustoTests/CalculadoraPrecoItemTests.cs
+++ Questao2/CalculoCustoTests/CalculadoraPrecoItemTests.cs
@@ -1,25 +1,38 @@
 namespace CalculoCustoTests
 {
     using HiperMercado;
+    using HiperMercado.TiposDeItems;
     using Xunit;
 
     public class CalculadoraPrecoItemTests
     {
-        [Theory]
-        [InlineData(50.0, 30, true, 10.0, false, 0, 0, 90.0)] // Item do tipo Refrigeração
-        [InlineData(100.0, 10, false, 0, true, 5.0, 2.0, 120.0)] // Item do tipo Volume
-        [InlineData(20.0, 20, false, 0, false, 0, 0, 40.0)] // Item Comum
-        public void CalcularPreco_ValidaPrecoCorretamente(double custo, int validade, bool ehRefrigeracao, double custoRefrigeracao, 
-            bool ehVolume, double volume, double custoPorVolume, double precoEsperado)
+        [Fact]
+        public void CalcularPreco_ValidaPrecoCorretamente_Quando_ItemRefrigeracao()
         {
             //Arrange
-            var item = new Item(custo, validade, ehRefrigeracao, custoRefrigeracao,
-                                ehVolume, volume, custoPorVolume);
+            var item = new ItemRefrigeracao(custo: 50.0, validade: 30, custoRefrigeracao: 10.0);
 
             var calculadoraDePreco = new CalculadoraPrecoItem();
 
             // Act
             var precoRetornado = calculadoraDePreco.CalcularPreco(item);
+            var precoEsperado = 140;
+
+            // Assert
+            Assert.Equal(precoEsperado, precoRetornado);
+        }
+        
+        [Fact]
+        public void CalcularPreco_ValidaPrecoCorretamente_Quando_ItemVolume()
+        {
+            //Arrange
+            var item = new ItemVolume(custo: 100.0, validade: 10, volume: 5.0, custoPorVolume: 2.0);
+
+            var calculadoraDePreco = new CalculadoraPrecoItem();
+
+            // Act
+            var precoRetornado = calculadoraDePreco.CalcularPreco(item);
+            var precoEsperado = 220;
 
             // Assert
             Assert.Equal(precoEsperado, precoRetornado);
