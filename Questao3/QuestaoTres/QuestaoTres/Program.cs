3870c1f85aac4cee259e2e2a61ffd32839f888c4
alynek <alyne0x67@gmail.com>
2024-07-03 12:44:29 -0300

    Cria outra abordagem para inserir casas em uma rua e retornar o número de eleitores

--- Questao3/QuestaoTres/QuestaoTres/Program.cs
+++ Questao3/QuestaoTres/QuestaoTres/Program.cs
@@ -1,58 +1,43 @@
-﻿/*
- Um candidato a prefeito quer saber quais ruas ele deve visitar para impactar o maior
-número de eleitores. Abaixo seguem objetos já existentes que representam casas e ruas:
- */
+﻿//Uma casa sempre será única (número + quantiade de eleitores) em uma rua, por isso o hashset
 
-var ruaDaSeda = new Rua { Cep = "12345-678", Nome = "Rua da seda" };
-var ruaDoAco = new Rua { Cep = "987654-321", Nome = "Rua do Aço" };
-var ruaDaFarinha = new Rua { Cep = "11111-212", Nome = "Rua da Farinha" };
-var ruaPortoReal = new Rua { Cep = "99999-111", Nome = "Rua Porto Real" };
-
-var casas = new List<Casa>
+var casasDaRuaDaSeda = new HashSet<Casa>
 {
-    new() { Rua = ruaDaSeda, Numero = 5, TotalEleitores = 100 },
-    new() { Rua = ruaDaSeda, Numero = 2, TotalEleitores = 150 },
-    new() { Rua = ruaDoAco, Numero = 1, TotalEleitores = 200 },
-    new() { Rua = ruaDaFarinha, Numero = 2, TotalEleitores = 50 },
-    new() { Rua = ruaDaFarinha, Numero = 3, TotalEleitores = 75 },
-    new() { Rua = ruaPortoReal, Numero = 3, TotalEleitores = 500 }
+    new() { Numero = 5, TotalEleitores = 100 },
+    new() { Numero = 2, TotalEleitores = 150 },
+    new() { Numero = 20, TotalEleitores = 150 },
+    new() { Numero = 1, TotalEleitores = 200 },
+    new() { Numero = 2, TotalEleitores = 50 },
+    new() { Numero = 3, TotalEleitores = 75 },
+    new() { Numero = 3, TotalEleitores = 500 }
 };
 
-var resultado = ProcessaAlgoritmo.RetornaAsRuasOrdenadasPelaQuantidadeDeEleitores(casas);
-
-resultado.ForEach(_ => Console.WriteLine($"Rua: {_.Nome}"));
-
-public static class ProcessaAlgoritmo
+var casasDaRuaDoAco = new HashSet<Casa>
 {
-    public static List<Rua> RetornaAsRuasOrdenadasPelaQuantidadeDeEleitores(List<Casa> casas)
-    {
-        var quantidadeEleitoresRuas = new Dictionary<Rua, int>();
+    new() { Numero = 5, TotalEleitores = 2 },
+    new() { Numero = 2, TotalEleitores = 3 },
+    new() { Numero = 20, TotalEleitores = 1 },
+};
 
-        casas.ForEach(_ =>
-        {
-            if (quantidadeEleitoresRuas.ContainsKey(_.Rua))
-            {
-                quantidadeEleitoresRuas[_.Rua] += _.TotalEleitores;
-            }
-            else
-            {
-                quantidadeEleitoresRuas[_.Rua] = _.TotalEleitores;
-            }
-        });
+var casasDaRuaDaFarinha = new HashSet<Casa>
+{
+    new() { Numero = 5, TotalEleitores = 6 },
+    new() { Numero = 2, TotalEleitores = 4 },
+    new() { Numero = 20, TotalEleitores = 4 },
+};
 
-        var ruasOrdenadasPelosEleitores = quantidadeEleitoresRuas
-           .OrderByDescending(chave => chave.Value)
-           .ThenBy(chave => chave.Key.Nome)
-           .Select(chave => chave.Key)
-           .ToList();
+var ruaDaSeda = new Rua("12345-678", "Rua da seda", casasDaRuaDaSeda);
+var ruaDoAco = new Rua("987654-321", "Rua do Aço", casasDaRuaDoAco);
+var ruaDaFarinha = new Rua("11111-212", "Rua da Farinha", casasDaRuaDaFarinha);
 
-        return ruasOrdenadasPelosEleitores;
-    }
+var listaDeRuas = new List<Rua>() { ruaDaSeda, ruaDoAco, ruaDaFarinha };
+
+foreach (var item in listaDeRuas)
+{
+    Console.WriteLine($"Nome da rua: {item.Nome}, quantidade ded eleitores: {item.QuantidadeEleitoresRuas}");
 }
 
-
-public class Casa { 
-    public Rua Rua { get; set; }
+public class Casa
+{
     public int Numero { get; set; }
     public int TotalEleitores { get; set; }
 }
@@ -61,6 +46,18 @@ public class Rua
 {
     public string Cep { get; set; }
     public string Nome { get; set; }
+
+    private HashSet<Casa> Casas { get; set; }
+
+    public int QuantidadeEleitoresRuas { get; private set; }
+
+    public Rua(string cep, string nome, HashSet<Casa> casas)
+    {
+        Cep = cep;
+        Nome = nome;
+        Casas = casas;
+        QuantidadeEleitoresRuas = casas.Select(_ => _.TotalEleitores).Sum();
+    }
 
     public override bool Equals(object obj)
     {
